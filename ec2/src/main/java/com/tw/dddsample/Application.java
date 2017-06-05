b5a9ffdc57037fb03a6c0c8afa507b772759e1e0
zhuao <zhuao1051@gmail.com>
2017-06-05 17:27:32 +0800

    rename yaml file extention

--- ec2/src/main/java/com/tw/dddsample/Application.java
+++ ec2/src/main/java/com/tw/dddsample/Application.java
@@ -12,7 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableAutoConfiguration
 @EnableEurekaClient
 @EnableSwagger2
-@ComponentScan(basePackages = {"com.tw.dddsample", "io.swagger", "com.example"})
+@ComponentScan(basePackages = {"com.tw.dddsample", "io.swagger"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
