6763b9e3a715cd8eae8e2f81a146c7f1a5697edb
whunmr <whunmr@gmail.com>
2017-06-05 14:47:52 +0800

    [rds] init rds

---
+++ rds/src/main/java/com/example/Application.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableAutoConfiguration
+@EnableEurekaClient
+@EnableSwagger2
+@ComponentScan(basePackages = {"io.swagger", "com.example"})
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
