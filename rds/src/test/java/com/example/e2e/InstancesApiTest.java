f29575ee8e114f147d0e1f803c48f1f1f13ebb93
zhuao <zhuao1051@gmail.com>
2017-06-07 01:09:45 +0800

    correct rest assured test

--- rds/src/test/java/com/example/e2e/InstancesApiTest.java
+++ rds/src/test/java/com/example/e2e/InstancesApiTest.java
@@ -1,27 +1,31 @@
 package com.example.e2e;
 
-import com.example.Application;
 import io.restassured.RestAssured;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
 
-import static com.example.e2e.TestUtil.propertyValue;
 import static io.restassured.RestAssured.given;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(webEnvironment = DEFINED_PORT, classes={Application.class}, properties = "")
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = DEFINED_PORT)
 public class InstancesApiTest {
+
+    @LocalServerPort
+    private int port;
+
     @Before
     public void setup() {
-        RestAssured.baseURI  = propertyValue("server.host", "http://localhost");
-        RestAssured.port     = propertyValue("server.port", 9000);
-        RestAssured.basePath = propertyValue("server.base", "/");
+        RestAssured.baseURI  = "http://localhost";
+        RestAssured.port     = port;
+        RestAssured.basePath = "/";
     }
 
     @Test
