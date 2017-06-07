e913c5fe6323688bd22b2d11283954a0b7d92b36
whunmr <whunmr@gmail.com>
2017-06-07 09:05:25 +0800

    [rds] refactor rds pact test

--- rds/src/main/resources/db/schema.sql
+++ rds/src/main/resources/db/schema.sql
@@ -1,3 +1,3 @@
 drop table INSTANCE if exists;
 
-create table INSTANCE (ID int, INSTANCE_ID varchar(36) not null, EC2INSTANCE_ID varchar(36) not null, INSTANCE_CLASS varchar(128) not null, ENGINE varchar(50), PORT int, STATUS varchar(50));
+create table INSTANCE (INSTANCE_ID varchar(36) not null, EC2INSTANCE_ID varchar(36) not null, INSTANCE_CLASS varchar(128) not null, ENGINE varchar(50), PORT int, STATUS varchar(50));
--- rds/src/test/java/com/example/interfaces/InstancesFacadeTest.java
+++ rds/src/test/java/com/example/interfaces/InstancesFacadeTest.java
@@ -37,20 +37,31 @@ import static org.junit.Assert.assertNotNull;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class InstancesFacadeTest {
+//    DslPart requestData = new PactDslJsonBody()
+//            .object("data")
+//                .object("attributes")
+//                    .stringValue("name", "create aa instance")
+//                    .object("plan")
+//                        .stringValue("image_id", "image____id__3")
+//                        .stringValue("flavor_id", "c1.medium")
+//                        .stringValue("az", "SZ")
+//                    .closeObject()
+//                .closeObject()
+//            .closeObject()
+//            .close();
+
     DslPart requestData = new PactDslJsonBody()
             .object("data")
                 .object("attributes")
-                    .stringValue("name", "create aa instance")
                     .object("plan")
-                        .stringValue("image_id", "image____id__3")
-                        .stringValue("flavor_id", "flavor_id1")
-                        .stringValue("az", "SZ")
+                        .stringValue("flavor_id", "c1.medium")
                     .closeObject()
                 .closeObject()
             .closeObject()
             .close();
 
     DslPart responseData = new PactDslJsonBody().object("data")
+                                    .stringValue("id", "fake_ec2_instance_uuid")
                                     .stringMatcher("id", "[0-9a-zA-Z\\-]+")
                                     .stringValue("type", "c1.medium")
                                     .object("attributes")
@@ -67,23 +78,6 @@ public class InstancesFacadeTest {
 //    @Rule
 //    public PactProviderRuleMk2 rule = new PactProviderRuleMk2("EC2", "localhost",9002, this);
 
-//    @Autowired
-//    private EurekaDiscoveryClient client;
-//
-//    @Bean
-//    public EurekaDiscoveryClient getClient() {
-//        EurekaDiscoveryClient mock = Mockito.mock(EurekaDiscoveryClient.class);
-//        LinkedList<ServiceInstance> t = new LinkedList<>();
-//        ServiceInstance serviceInstance = Mockito.mock(ServiceInstance.class);
-//        when(serviceInstance.getHost()).thenReturn("localhost");
-//        when(serviceInstance.getPort()).thenReturn(9002);
-//        when(serviceInstance.getServiceId()).thenReturn("EC2");
-//        t.add(serviceInstance);
-//        when(mock.getInstances("EC2")).thenReturn(t);
-//        return mock;
-//    }
-
-
     @Autowired
     private InstancesFacade instancesFacade;
 
@@ -96,26 +90,22 @@ public class InstancesFacadeTest {
                 .method(HttpMethod.POST)
                 .path("/instances")
                 .willRespondWith()
-                .body(requestData)
+                .body(responseData)
                 .toPact();
     }
 
-//    @Test
-//    @PactVerification("EC2")
+
     public void should_create_an_mysql_db_instance_successfully() {
-        ResponseEntity<CreateDBInstanceResponse> response = instancesFacade.createInstance(new CreateDBInstanceRequest().instanceClass("c1.medium").engine("MySQL"));
+        ResponseEntity<CreateDBInstanceResponse> response = instancesFacade.createInstance(new CreateDBInstanceRequest().instanceId("my_rds_mysql_instance1").instanceClass("c1.medium").engine("MySQL"));
         assertEquals(response.getStatusCode(), HttpStatus.OK);
         assertEquals(response.getBody().getInstance().getEngine(), "MySQL");
         assertEquals(response.getBody().getInstance().getInstanceClass(), "c1.medium");
         assertNotNull(response.getBody().getInstance().getInstanceId());
-
     }
 
     @Test
     public void testPact() {
-        PactDslWithProvider pactDslWithProvider = ConsumerPactBuilder
-                .consumer("Some Consumer")
-                .hasPactWith("Some Provider");
+        PactDslWithProvider pactDslWithProvider = ConsumerPactBuilder.consumer("RDS").hasPactWith("EC2");
 
         RequestResponsePact pact = create_ec2_instance_when_rds_create_db_instance(pactDslWithProvider);
 
